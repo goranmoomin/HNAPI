4f2afc78ed75515555b8773078d77e3a0f569e32
Sungbin Jo <pcr910303@icloud.com>
2020-04-10 02:21:32 +0900

    Split Algolia API client loading code into methods

--- Sources/AlgoliaAPIClient.swift
+++ Sources/AlgoliaAPIClient.swift
@@ -10,28 +10,37 @@ class AlgoliaAPIClient {
 
     struct QueryResult: Decodable { var hits: [TopLevelItem] }
 
-    func topItems(
-        count: Int = 30, completionHandler: @escaping (Result<[TopLevelItem], Error>) -> Void
+    func items(ids: [Int], completionHandler: @escaping (Result<[TopLevelItem], Error>) -> Void) {
+        networkClient.request(to: .algolia(ids: ids)) { result in
+            guard case let .success((data, _)) = result else {
+                completionHandler(.failure(result.failure!))
+                return
+            }
+            let itemsResult = Result<[TopLevelItem], Error> {
+                let queryResult = try self.decoder.decode(QueryResult.self, from: data)
+                return queryResult.hits
+            }
+            completionHandler(itemsResult)
+        }
+    }
+
+    func items(
+        category: Category = .top, count: Int = 30,
+        completionHandler: @escaping (Result<[TopLevelItem], Error>) -> Void
     ) {
-        networkClient.request(to: .firebase(category: .top)) { result in
+        networkClient.request(to: .firebase(category: category)) { result in
             guard case let .success((data, _)) = result else {
                 completionHandler(.failure(result.failure!))
                 return
             }
             do {
                 let ids = Array(try self.decoder.decode([Int].self, from: data).prefix(count))
-                self.networkClient.request(to: .algolia(ids: ids)) { result in
-                    guard case let .success((data, _)) = result else {
-                        completionHandler(.failure(result.failure!))
-                        return
-                    }
-                    let itemsResult = Result<[TopLevelItem], Error> {
-                        let queryResult = try self.decoder.decode(QueryResult.self, from: data)
-                        return queryResult.hits
-                    }
-                    completionHandler(itemsResult)
-                }
+                self.items(ids: ids, completionHandler: completionHandler)
             } catch { completionHandler(.failure(error)) }
         }
     }
+
+    func topItems(
+        count: Int = 30, completionHandler: @escaping (Result<[TopLevelItem], Error>) -> Void
+    ) { items(category: .top, count: count, completionHandler: completionHandler) }
 }
