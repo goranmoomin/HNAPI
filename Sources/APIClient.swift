534b1d3602af7adfd6caf654b88d7cbc5f78b409
Sungbin Jo <goranmoomin@daum.net>
2021-07-23 15:44:20 +0900

    Add User-Agent header to network client

--- Sources/APIClient.swift
+++ Sources/APIClient.swift
@@ -14,6 +14,10 @@ public class APIClient {
     var networkClient: NetworkClient = {
         let configuration = URLSessionConfiguration.ephemeral
         configuration.httpShouldSetCookies = false
+        configuration.httpAdditionalHeaders = [
+            "User-Agent":
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15"
+        ]
         return URLSession(configuration: configuration)
     }()
 
