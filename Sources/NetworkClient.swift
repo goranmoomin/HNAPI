a80026617fc1834dd1558eba3aa299049c825c10
Sungbin Jo <goranmoomin@daum.net>
2021-07-25 15:03:38 +0900

    Add error handling for 4xx, 5xx status codes

--- Sources/NetworkClient.swift
+++ Sources/NetworkClient.swift
@@ -7,15 +7,23 @@ protocol NetworkClient {
 }
 
 extension URLSession: NetworkClient {
+    public enum HTTPError: Error {
+        case transportError(Error)
+        case serverSideError(statusCode: Int)
+    }
+
     fileprivate static func adapter(_ completionHandler: @escaping Completion) -> (
         Data?, URLResponse?, Error?
     ) -> Void {
         let adapter = { (data: Data?, response: URLResponse?, error: Error?) in
-            if let data = data, let response = response {
-                // FIXME: Error handling
-                completionHandler(.success((data, response as! HTTPURLResponse)))
+            if let data = data, let response = response as? HTTPURLResponse {
+                if response.statusCode >= 400 && response.statusCode < 500 {
+                    completionHandler(.failure(HTTPError.serverSideError(statusCode: response.statusCode)))
+                } else {
+                    completionHandler(.success((data, response)))
+                }
             } else if let error = error {
-                completionHandler(.failure(error))
+                completionHandler(.failure(HTTPError.transportError(error)))
             } else {
                 preconditionFailure("Data and Error can't both be nil")
             }
